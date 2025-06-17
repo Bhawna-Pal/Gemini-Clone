3d99e7213f4f2425eae4edde1906e8a67911a2f1
Bhawna Pal <bhawnapal2104@gmail.com>
2025-06-17 17:19:16 +0530

    Initial commit

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Sidebar.css'
+import {assets} from '../../assets/assets'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+        <img className='menu' src={assets.menu_icon} alt="" />
+        <div className="new-chat"></div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default Sidebar
